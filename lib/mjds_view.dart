b77fb3d88ee1ffcc5d0fc37a54ef0ef17dc07e29
Majd Alghamdi <majd.alghamdi@globant.com>
2024-10-17 11:37:06 +0300

    mjd commit

---
+++ lib/mjds_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MjdView extends StatelessWidget {
+  const MjdView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Hi from the new view"),
+      ),
+    );
+  }
+}
