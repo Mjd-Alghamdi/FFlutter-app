a56d29a2cfb85a3912bc22d028569bfce9b9e1b1
Majd Alghamdi <majd.alghamdi@globant.com>
2024-10-22 16:03:50 +0300

    Add action button in dialog

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -113,14 +113,28 @@ class _HomeState extends State<HomeView> {
       showCupertinoModalPopup(
         context: context,
         builder: (context) {
-          return const CupertinoAlertDialog(
-            content: Text(
+          return CupertinoAlertDialog(
+            content: const Text(
               "This a Flutter lab!",
               style: TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
               ),
             ),
+            actions: [
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Ok',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
           );
         },
       );
